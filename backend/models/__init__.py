92a5c2bf2c13407c80621fcc69fc95029caa5e0b
Sebastián Alexander <129068618+SebAlexanderLG@users.noreply.github.com>
2025-11-21 11:54:55 -0300

    Delete backend directory

--- backend/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .usuario import Usuario  # noqa: F401
-from .remitente import Remitente  # noqa: F401
-from .prompt import Prompt  # noqa: F401
-from .email import Email  # noqa: F401
-from .token import GmailToken  # noqa: F401
--- backend/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-from .extractores import extraer_encabezados  # noqa: F401
-from .extractores import extraer_asunto  # noqa: F401
-from .extractores import extraer_remitente  # noqa: F401
-from .payloads import procesar_payload  # noqa: F401
-from .extractores import extraer_fecha_correo  # noqa: F401
-from .correos import descargar_adjunto_gmail  # noqa: F401
-from .correos import manejo_credenciales  # noqa: F401
